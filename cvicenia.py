54b987db04e62aea8e6a4afe4a881f79b6029354
Mims <miriama.romanov@gmail.com>
2024-04-23 10:27:56 +0200

    pridanie cviceni

---
+++ cvicenia.py
@@ -0,0 +1,23 @@
+#vypise hviezdicky tri rady do dvoch stlpcoch
+rows = 3
+columns = 2
+while rows < 6:
+    print ("**", end ="\n")
+    rows += 1
+
+ #vypise vysledok syntaxu ((5 + 2) - 3))
+def addition (a,b,c):
+    result = ((a + b) - c)
+    return result
+
+solusion = addition (a =5,b=2,c=3)
+final = solusion 
+print(final)
+
+# pomocou datoveho typu tuple, syntax prikladu 4 + 1 a 4 - 1  
+def pack_tuple (cislo1, cislo2):
+    x = cislo1 + cislo2
+    y = cislo1 - cislo2
+    return x, y
+z = pack_tuple(4,1)
+print(z)
