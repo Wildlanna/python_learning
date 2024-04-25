1aaad71e576418ae9d9616fa6f2a61a653fb444e
Mims <miriama.romanov@gmail.com>
2024-04-25 23:03:55 +0200

    commit_dict

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+# dictionaries
+person = {
+    "name": "Mims", 
+    "surname" : "Romanova",
+    "age" : 29,
+    "boyfriend": "Radko",
+    "languages": ["Python", "JavaScript"],
+    "instagram": "Mims_ka",
+    "Facebook" : "Mims_ka",
+       "address": {
+        "city" : "Kosice",
+        "street" : "Tahanovce",
+        "street_number" : 23
+    }
+}
+print(person ["address"] ["city"])
